92f0e6f038686dc6e654e60cd549fea1987bcd52
Maria Luísa Riolino Guimarães <maria.guimaraes@estudante.ufjf.br>
2022-10-21 17:47:08 -0300

    deleta arquivos main

--- dcc012b-trab2-team-100-palavras-main/sempalavras/include/config.h
+++
@@ -1,3 +0,0 @@
-#define CPU_TIME
-#define INPUT_DIR "/home/marialuisa/dcc012/trab2/dcc012b-trab2-team-100-palavras/input/"
-#define OUTPUT_DIR "/home/marialuisa/dcc012/trab2/dcc012b-trab2-team-100-palavras/output/"
